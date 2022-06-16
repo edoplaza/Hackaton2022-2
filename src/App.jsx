3cfe1bbe49cdfde634964d66aa95b7cb4fad4de9
Eduardo Plaza <edoplaza@gmail.com>
2022-06-16 16:01:11 +0200

    add icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import React, { useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 
-import Counties from './assets/counties.json'
-
+import Counties from "./assets/data/counties.json";
+import Cemeteries from "./assets/data/cemeteries.json";
+import tombstone from "./assets/icons/tombstone.png";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
@@ -19,53 +20,51 @@ const App = () => {
     });
 
     map.on("load", () => {
-
       //Counties
       map.addSource("counties", {
         type: "geojson",
         data: Counties,
       });
 
-      map.addLayer({
-        id: "lithuanian-counties",
-        type: "fill",
-        source: "counties",
-        paint: {
-          "fill-color": "#ffffff",
-          "fill-opacity": 0.8,
-          'fill-outline-color': 'red'
-        },
-        filter: ["==", "$type", "Polygon"],
-      });
+      // map.addLayer({
+      //   id: "lithuanian-counties",
+      //   type: "fill",
+      //   source: "counties",
+      //   paint: {
+      //     "fill-color": "#ffffff",
+      //     "fill-opacity": 0.3,
+      //     'fill-outline-color': 'red'
+      //   },
+      //   filter: ["==", "$type", "Polygon"],
+      // });
 
       //Cemeteries
-      map.addSource("cemeteries", {
-        type: "geojson",
-        data: {
-          type: "FeatureCollection",
-          features: [
-            {
-              type: "Feature",
-              geometry: {
-                type: "Point",
-                coordinates: [ 25.238212, 54.706052],
-              },
-            },
-          ]
-        }
-      })
+      map.loadImage(tombstone, (error, image) => {
+        if (error) throw error;
+        map.addImage("tombstone", image);
+        map.addSource("point", {
+          type: "geojson",
+          data: Cemeteries,
+        });
 
-      map.addLayer({
-        id: "vilinius-cemeteries",
-        type: "circle",
-        source: "cemeteries",
-        paint: {
-          "circle-radius": 6,
-          "circle-color": "#0000ff",
-        },
-        filter: ["==", "$type", "Point"],
+        map.addLayer({
+          id: "points",
+          type: "symbol",
+          source: "point",
+          layout: {
+            "icon-image": "tombstone",
+            "icon-size": 0.25,
+            "text-field": ["get", "title"],
+            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+            "text-size": 12,
+            "text-offset": [0, 2],
+            "text-anchor": "bottom",
+          },
+          paint: {
+            "text-color": "#ffffff"
+          }
+        });
       });
-      
     });
 
     return () => map.remove();
