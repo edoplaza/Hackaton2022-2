a934de72ce1d591d9c1805e0e51c42187511b509
Eduardo Plaza <edoplaza@gmail.com>
2022-06-16 18:08:52 +0200

    refine ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,16 @@
-import React, { useRef, useEffect } from "react";
-import mapboxgl from "mapbox-gl";
-import "mapbox-gl/dist/mapbox-gl.css";
-
-import Counties from "./assets/data/counties.json";
-import Cemeteries from "./assets/data/cemeteries.json";
-import tombstone from "./assets/icons/tombstone.png";
-
-mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
+import React from "react";
+import Map from './components/Map'
+import Sidebar from './components/Sidebar'
 
 const App = () => {
-  const mapContainer = useRef();
+  return (
+    <div className="App">
+      <Sidebar/>
+      <Map/>
+    </div>
+  )
 
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/dark-v10",
-      center: [25.279652, 54.687157],
-      zoom: 10,
-    });
-
-    map.on("load", () => {
-      //Counties
-      map.addSource("counties", {
-        type: "geojson",
-        data: Counties,
-      });
-
-      // map.addLayer({
-      //   id: "lithuanian-counties",
-      //   type: "fill",
-      //   source: "counties",
-      //   paint: {
-      //     "fill-color": "#ffffff",
-      //     "fill-opacity": 0.3,
-      //     'fill-outline-color': 'red'
-      //   },
-      //   filter: ["==", "$type", "Polygon"],
-      // });
-
-      //Cemeteries
-      map.loadImage(tombstone, (error, image) => {
-        if (error) throw error;
-        map.addImage("tombstone", image);
-        map.addSource("point", {
-          type: "geojson",
-          data: Cemeteries,
-        });
-
-        map.addLayer({
-          id: "points",
-          type: "symbol",
-          source: "point",
-          layout: {
-            "icon-image": "tombstone",
-            "icon-size": 0.25,
-            "text-field": ["get", "title"],
-            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-            "text-size": 12,
-            "text-offset": [0, 2],
-            "text-anchor": "bottom",
-          },
-          paint: {
-            "text-color": "#ffffff"
-          }
-        });
-      });
-    });
-
-    return () => map.remove();
-  }, []);
-
-  return <div ref={mapContainer} style={{ width: "100%", height: "100vh" }} />;
+ 
 };
 
 export default App;
---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,12 @@
+const Sidebar = () => {
+
+  return (
+    <div className="Sidebar" data-testid="Sidebar">
+      <button id="bars-layer" className="Sidebar__button Sidebar__button--on">Bars</button>
+      <button id="clubs-layer" className="Sidebar__button Sidebar__button--on">Strip Clubs</button>
+      <button id="cemeteries-layer" className="Sidebar__button Sidebar__button--on">Cemeteries</button>
+    </div>
+  )
+}
+
+export default Sidebar
