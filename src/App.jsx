9e35cff9290245092ceafa398f0508221e3f9df1
Eduardo Plaza <edoplaza@gmail.com>
2022-06-16 13:06:27 +0200

    add example dataset and layer

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,34 @@ const App = () => {
       center: [25.279652, 54.687157],
       zoom: 10,
     })
+
+    map.on("load", () => {
+      console.log('loaded')
+
+      map.addSource("bus-routes", {
+        type: "geojson",
+        data:
+          "https://opendata.arcgis.com/datasets/4347f3565fbe4d5dbb97b016768b8907_0.geojson",
+      })
+
+      map.addLayer({
+        id: "bus-routes-line",
+        type: "line",
+        source: "bus-routes",
+        paint: {
+          "line-color": "#15cc09",
+          "line-width": 4,
+        },
+      })
+    })
+
     return () => map.remove()
 
   }, [])
+
+
+
+
   return <div ref={mapContainer} style={{ width: "100%", height: "100vh" }} />
 
 }
